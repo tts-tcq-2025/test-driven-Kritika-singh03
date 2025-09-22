b6aa4a045b01549fd3f62b54c44b6c66f90ab179
Kritika-singh03 <kritika.singh@in.bosch.com>
2025-09-23 00:10:38 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,6 +2,8 @@
 #include <string>
 
 int StringCalculator::Add(const std::string& numbers) {
+    if (numbers.empty()) return 0;  //  Step 1: minimal code for first test
+    return 0;  //  fallback, will evolve as we add tests
     if (numbers.empty()) return 0;        //  Step 1: minimal code for first test
     return 0;          //  fallback, will evolve as we add tests
 }
