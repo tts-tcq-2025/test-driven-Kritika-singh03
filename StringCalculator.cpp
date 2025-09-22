b364b4b785f0ec4891f861e473563c59a207227b
Kritika-singh03 <kritika.singh@in.bosch.com>
2025-09-22 23:08:45 +0530

    Create StringCalculator.cpp

---
+++ StringCalculator.cpp
@@ -0,0 +1,6 @@
+#include "StringCalculator.h"
+
+int StringCalculator::Add(const std::string& numbers) {
+    if (numbers.empty()) return 0;  // Step 1: minimal code for first test
+    return 0; // fallback, will evolve as we add tests
+}
